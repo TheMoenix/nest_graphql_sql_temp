6cf1412a189c48e2d683448eb6c97b49d33fb533
Mohammed Alshami <alshami@juleb.com>
2023-04-09 02:11:00 +0300

    sql added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'moenix',
+      entities: [],
+      synchronize: false,
+      retryAttempts: 3,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, Logger],
 })
